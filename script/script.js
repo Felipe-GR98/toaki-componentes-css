8b15492a52bb6c7d3f9e3c895a834e6b960327d5
unknown <davivarejao@gmail.com>
2025-11-09 18:05:59 -0300

    retira comentários e adiciona logica de customizar icone

--- script/script.js
+++ script/script.js
@@ -39,21 +39,64 @@ function pegarEstilos(e){
 } */
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('página carregou!');
-  
   const componentes = document.getElementsByClassName('componente');
-  console.log('selecionei todos os componentes: ', componentes);
-  
-  const listaComponentes = Array.from(componentes);
-  console.log('converti todos os componentes', listaComponentes);
-  
-  listaComponentes.forEach((componente) => {
-    console.log('adicionando o evento click no componente', componente);
-    
+
+  Array.from(componentes).forEach((componente) => {
     componente.addEventListener('click', () => {
       console.log('adicionei o evento no componente: ', componente);
     });
   });
-  console.log('adicionei todos os eventos!');
+
+  const listaIcones = [
+    { nome: "Retorno", classe: "icone-retorno" },
+    { nome: "Chat", classe: "icone-chat" },
+    { nome: "Estrela", classe: "icone-estrela" },
+    { nome: "Fechar", classe: "icone-fechar" },
+    { nome: "Lupa", classe: "icone-lupa" },
+    { nome: "Mapa", classe: "icone-mapa" },
+    { nome: "Tag", classe: "icone-tag" },
+    { nome: "Notas", classe: "icone-notas" },
+    { nome: "Telefone", classe: "icone-telefone" },
+    { nome: "Usuario", classe: "icone-usuario" },
+    { nome: "Home", classe: "icone-home" },
+    { nome: "Email", classe: "icone-email" },
+    { nome: "Calendário", classe: "icone-calendario" },
+    { nome: "Editar", classe: "icone-editar" },
+    { nome: "Documento", classe: "icone-documento" },
+    { nome: "Amendoim", classe: "icone-amendoim" },
+    { nome: "Roupa", classe: "icone-roupa" },
+    { nome: "Picolé", classe: "icone-picole" },
+    { nome: "Óculos", classe: "icone-oculos" },
+    { nome: "Camarão", classe: "icone-camarao" },
+    { nome: "Bolsa de compras", classe: "icone-bolsa-de-compras" },
+    { nome: "Relógio", classe: "icone-relogio" },
+    { nome: "Sino", classe: "icone-sino" },
+    { nome: "Enviar", classe: "icone-enviar" },
+    { nome: "Centralizar", classe: "icone-centralizar" },
+    { nome: "Mais", classe: "icone-mais" },
+    { nome: "Menu", classe: "icone-menu" },
+    { nome: "Coração", classe: "icone-coracao" },
+    { nome: "BOLA", classe: "icone-bola" }
+  ];
+
+  const selectTamanhoIcone = document.getElementById('icone-tamanho');
+  const selectTipoIcone = document.getElementById('tipo-icone');
+
+  listaIcones.forEach((icone) => {
+    const option = document.createElement('option');
+    option.value = icone.classe;
+    option.innerText = icone.nome;
+    selectTipoIcone.appendChild(option);
+  })
+
+  const icone = document.getElementById('customizacao-icone').getElementsByTagName('svg')[0];
+
+  selectTamanhoIcone.addEventListener('change', () => {
+    icone.classList.replace(icone.classList[0], selectTamanhoIcone.value)
+  });
+
+  selectTipoIcone.addEventListener('change', () => {
+    icone.classList.replace(icone.classList[1], selectTipoIcone.value)
+  });
 
 });
